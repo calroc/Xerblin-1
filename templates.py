e44020b7c55f8e8293ca1cafe3717d72fc2d7e50
Simon Forman <forman.simon@gmail.com>
2013-07-06 20:35:44 -0700

    Serve and use JS (D3 and jQuery)

--- templates.py
+++ templates.py
@@ -75,17 +75,50 @@ def display_interpreter(c, (stack, dictionary)):
       f.input(type_='submit', value='interpret')
 
 
+def use_js(h):
+  h.script(src='/static/jquery-1.10.2.min.js', charset='utf-8')
+  h.script(src='/static/d3.v3.min.js', charset='utf-8')
+
+
 def render(interpreter):
   ht = HTML()
-  title = key_get(interpreter[1], 'title', 'A Title')
+  d = interpreter[1]
+  title = key_get(d, 'title', 'A Title')
   with ht.head as h:
     h.title(title)
     h.meta(charset='utf-8')
     h.link(rel='stylesheet', href='./static/site.css')
-    h.script(src='/static/d3.v3.js', charset='utf-8')
+    extra_head = key_get(d, 'extra_head')
   with ht.body as b:
     b.h1(title)
+    b.h3('A DeltaBot')
+    b.div(id_='viewer', style='float:left').svg(width='1000px', height='128px')
     display_interpreter(b, interpreter)
+    b.script('''
+var data;
+var svg;
+
+
+$(function() {
+
+data = [
+    {x:23, y:18},
+    {x:99, y:18},
+    {x:23, y:28},
+    {x:99, y:28},
+    ];
+
+svg = d3.select("svg");
+svg.selectAll("circle")
+    .data(data)
+    .enter().append("svg:circle")
+    .attr("cx", function(d) {return d.x})
+    .attr("cy", function(d) {return d.y})
+    .attr("r", "3px")
+    ;
+
+})
+''')
   return ht
 
 
